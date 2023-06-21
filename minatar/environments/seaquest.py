43ebfedc6011ad92804e9d3caaefc76beecac044
florentdelgrange <florent.delgrange@gmail.com>
2023-06-21 16:19:15 +0200

    fixed state shape

--- minatar/environments/seaquest.py
+++ minatar/environments/seaquest.py
@@ -323,7 +323,7 @@ class Env(POPGymEnv):
 
     # Dimensionality of the game-state (10x10xn)
     def state_shape(self):
-        return [10,10,len(self.channels)]
+        return [10,10,len(self.channels_to_keep)]
 
     # Subset of actions that actually have a unique impact in this environment
     def minimal_action_set(self):
